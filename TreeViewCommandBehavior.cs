a542bc5676c55a206f47982c57477bc135ee15e5
Elio <elio.rbg@gmail.com>
2017-10-18 05:14:55 -0400

    Add files via upload

---
+++ TreeViewCommandBehavior.cs
@@ -0,0 +1,78 @@
+﻿using Apollo.SM.Core;
+using Prism.Interactivity;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Apollo.Infrastructure.Prism
+{
+    public class TreeViewCommandBehavior : CommandBehaviorBase<TreeView>
+    {
+        public TreeViewCommandBehavior(TreeView tree)
+            : base(tree)
+        {           
+            //tree.SelectedItemChanged += TreeView_SelectedItemChanged;
+            tree.MouseLeftButtonUp += Tree_MouseLeftButtonUp;
+        }
+
+        private void Tree_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            var tree = sender as TreeView;
+            if (null == tree || tree.SelectedItem == null)
+                return;
+            var menuItem = tree.SelectedItem as ModuleMenu;
+            if (!menuItem.CanNavigate)
+                return;
+
+            if (menuItem != null)
+                CommandParameter = menuItem;
+            else
+                CommandParameter = null;
+
+            ExecuteCommand(menuItem);
+        }
+
+        private void TreeView_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            //if (!System.Threading.Thread.CurrentPrincipal.Identity.IsAuthenticated)
+            //    return;
+            var tree = sender as TreeView;
+            
+            if ( null == tree || tree.SelectedItem == null)
+                return;
+
+            //var param = e.NewActiveTreeNode.Data as INavigationItem;
+            var param = e.NewValue as ModuleMenu;
+            if (!param.CanNavigate)
+                return;
+
+            if (param != null)
+                CommandParameter = param;
+            //CommandParameter = param.ViewName;
+            //CommandParameter = param.NavigationPath;
+            else
+                CommandParameter = null;
+
+            ExecuteCommand(param);
+  
+        }
+
+        //void TreeView_SelectedItemChanged(object sender, ActiveNodeChangedEventArgs e)
+        //{
+        //    //if (!System.Threading.Thread.CurrentPrincipal.Identity.IsAuthenticated)
+        //    //    return;
+
+        //    if (e.NewActiveTreeNode == null)
+        //        return;
+
+        //    //var param = e.NewActiveTreeNode.Data as INavigationItem;
+        //    var param = e.NewActiveTreeNode.Data as ModuleMenu;
+        //    if (param != null)
+        //        CommandParameter = param.ViewName;
+        //        //CommandParameter = param.NavigationPath;
+        //    else
+        //        CommandParameter = String.Empty;
+
+        //    ExecuteCommand(CommandParameter);
+        //}
+    }
+}
---
+++ TreeViewItemSelected.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Practices.Unity;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Apollo.Infrastructure.Prism
+{
+    public class TreeViewItemSelected
+    {
+        private static readonly DependencyProperty SelectedCommandBehaviorProperty =
+            DependencyProperty.RegisterAttached("SelectedCommandBehavior", typeof(TreeViewCommandBehavior), typeof(TreeViewItemSelected), null);
+
+        public static readonly DependencyProperty CommandProperty = 
+            DependencyProperty.RegisterAttached("Command", typeof(ICommand), typeof(TreeViewItemSelected), new PropertyMetadata(OnSetCommandCallback));
+        public static ICommand GetCommand(TreeView menuItem)
+        {
+            return menuItem.GetValue(CommandProperty) as ICommand;
+        }
+        public static void SetCommand(TreeView menuItem, ICommand command)
+        {
+            menuItem.SetValue(CommandProperty, command);
+        }
+
+        private static void OnSetCommandCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs e)
+        {
+            TreeView menuItem = dependencyObject as TreeView;
+            if (menuItem != null)
+            {
+                TreeViewCommandBehavior behavior = GetOrCreateBehavior(menuItem);
+                behavior.Command = e.NewValue as ICommand;
+            }
+        }
+
+        public static readonly DependencyProperty CommandParameterProperty = DependencyProperty.RegisterAttached("CommandParameter", typeof(object), typeof(TreeViewItemSelected), new PropertyMetadata(OnSetCommandParameterCallback));
+        public static object GetCommandParameter(TreeView menuItem)
+        {
+            return menuItem.GetValue(CommandParameterProperty);
+        }
+        public static void SetCommandParameter(TreeView menuItem, object parameter)
+        {
+            menuItem.SetValue(CommandParameterProperty, parameter);
+        }
+
+        private static void OnSetCommandParameterCallback(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs e)
+        {
+            TreeView menuItem = dependencyObject as TreeView;
+            if (menuItem != null)
+            {
+                TreeViewCommandBehavior behavior = GetOrCreateBehavior(menuItem);
+                behavior.CommandParameter = e.NewValue;
+            }
+        }
+
+        private static TreeViewCommandBehavior GetOrCreateBehavior(TreeView menuItem)
+        {
+            TreeViewCommandBehavior behavior = menuItem.GetValue(SelectedCommandBehaviorProperty) as TreeViewCommandBehavior;
+            if (behavior == null)
+            {
+                behavior = new TreeViewCommandBehavior(menuItem);
+                menuItem.SetValue(SelectedCommandBehaviorProperty, behavior);
+            }
+
+            return behavior;
+        }
+    }
+}

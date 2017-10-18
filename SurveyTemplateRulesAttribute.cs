fc662e180045033756bbcc766276594f397a9a88
Elio <elio.rbg@gmail.com>
2017-10-17 20:11:15 -0400

    Add files via upload

---
+++ SurveyTemplateRulesAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace Apollo.Survey.Core
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    public class SurveyTemplateRulesAttribute: ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext context)
+        {
+            if (value is SurveyTemplate)
+            {
+                int total = ((SurveyTemplate)value).Details
+                    .Where(e => !e.IsDeletedState)
+                    .Sum(e => e.Weight);
+
+                if (total == 100)
+                    return ValidationResult.Success;
+                else
+                    return new ValidationResult("The sum of the weights of the Details should be equal to 100.");
+
+            }
+            else
+                return ValidationResult.Success;
+        }
+
+    }
+}
